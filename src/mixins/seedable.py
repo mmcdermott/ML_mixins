d30296dae2ec2ed2c92b39206017a69ba5fca122
Matthew McDermott <mattmcdermott8@gmail.com>
2024-10-14 12:15:48 -0400

    Added some new tests.

--- src/mixins/seedable.py
+++ src/mixins/seedable.py
@@ -7,41 +7,104 @@ from datetime import datetime
 
 import numpy as np
 
+try:
+    import torch
+
+    def seed_torch(seed: int):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+except ModuleNotFoundError:
+
+    def seed_torch(seed: int):
+        pass
+
+
 from .utils import doublewrap
 
 
-def seed_everything(seed: int | None = None, try_import_torch: bool | None = True) -> int:
-    max_seed_value = np.iinfo(np.uint32).max
-    min_seed_value = np.iinfo(np.uint32).min
+def seed_everything(seed: int | None = None) -> int:
+    """A simple helper function to seed everything that needs to be seeded.
+
+    Args:
+        seed: The seed to use. If None, a random seed is chosen.
+
+    Returns:
+        The seed that was used.
+
+    Examples:
+        >>> random.seed(0)
+        >>> np.random.seed(0)
+        >>> random.randint(0, 10)
+        6
+        >>> random.randint(0, 10)
+        6
+        >>> np.random.randint(0, 10)
+        5
+        >>> np.random.randint(0, 10)
+        0
+        >>> seed_everything(0)
+        0
+        >>> random.randint(0, 10)
+        6
+        >>> random.randint(0, 10)
+        6
+        >>> np.random.randint(0, 10)
+        5
+        >>> np.random.randint(0, 10)
+        0
+    """
 
     try:
         if seed is None:
             seed = os.environ.get("PL_GLOBAL_SEED")
         seed = int(seed)
     except (TypeError, ValueError):
+        max_seed_value = np.iinfo(np.uint32).max
+        min_seed_value = np.iinfo(np.uint32).min
         seed = np.random.randint(min_seed_value, max_seed_value)
-
-    assert min_seed_value <= seed <= max_seed_value
 
     random.seed(seed)
     np.random.seed(seed)
-    if try_import_torch:
-        try:
-            import torch
-
-            torch.manual_seed(seed)
-            torch.cuda.manual_seed_all(seed)
-        except ModuleNotFoundError:
-            pass
+    seed_torch(seed)
 
     return seed
 
 
 class SeedableMixin:
+    """This class provides easy utilities to reliably seed stochastic processes.
+
+    This seeding can be used to ensure reproducibility in experiments, both in individual examples with an
+    integral seed or in a stochastic process both at a per-event level and at a whole process level by seeding
+    with `None`, in which case a new seed is chosen for each event in the process based on the prior seed and
+    stored.
+    """
+
     def __init__(self, *args, **kwargs):
         self._past_seeds = kwargs.get("_past_seeds", [])
 
-    def _last_seed(self, key: str):
+    def _last_seed(self, key: str) -> tuple[int, int | None]:
+        """This returns the most recently used seed with a given key.
+
+        Args:
+            key: The key to search for.
+
+        Returns:
+            The index of the most recent seed with a given key in the list of past seeds and the seed itself.
+
+        Examples:
+            >>> M = SeedableMixin()
+            >>> _ = M._seed(0, "foo")
+            >>> _ = M._seed(2, "bar")
+            >>> _ = M._seed(4, "foo")
+            >>> _ = M._seed(6, "baz")
+            >>> M._last_seed("foo")
+            (2, 4)
+            >>> M._last_seed("bar")
+            (1, 2)
+            >>> M._last_seed("baz")
+            (3, 6)
+        """
         for idx, (s, k, time) in enumerate(self._past_seeds[::-1]):
             if k == key:
                 idx = len(self._past_seeds) - 1 - idx
@@ -49,7 +112,46 @@ class SeedableMixin:
 
         return -1, None
 
-    def _seed(self, seed: int | None = None, key: str | None = None):
+    def _seed(self, seed: int | None = None, key: str | None = None) -> int:
+        """This seeds the random number generators.
+
+        Args:
+            seed: The seed to use. If None, a new seed is chosen.
+            key: The key to associate with this seed.
+
+        Returns:
+            The seed that was used.
+
+        Examples:
+            >>> M = SeedableMixin()
+            >>> M._seed(0, "foo")
+            0
+            >>> M._seed(2, "bar")
+            2
+            >>> M._seed(4, "foo")
+            4
+
+        Note that by virtue of the fact that we've already seeded `M`, future seeds are deterministic (though
+        they are still pseudo-random, as they are simply random integers drawn from the current random
+        distribution, which in this test was seeded at 4 immediately prior to this call).
+            >>> M._seed()
+            31681838
+
+        Past seeds and keys are stored in the `_past_seeds` attribute, which is created if the object does not
+        have it at the start.
+            >>> M = SeedableMixin()
+            >>> del M._past_seeds
+            >>> M._seed(0, "foo")
+            0
+            >>> M._seed(2, "bar")
+            2
+            >>> M._seed(4, "foo")
+            4
+            >>> M._seed()
+            31681838
+            >>> M._past_seeds
+            [(0, 'foo', ...), (2, 'bar', ...), (4, 'foo', ...), (31681838, '', ...)]
+        """
         if seed is None:
             seed = random.randint(0, int(1e8))
         if key is None:
@@ -67,7 +169,46 @@ class SeedableMixin:
 
     @staticmethod
     @doublewrap
-    def WithSeed(fn, key: str | None = None):
+    def WithSeed(fn, key: str | None = None) -> callable:
+        """This function is a decorator that returns a function that also takes a seed which seeds the RNG.
+
+        This decorator can either be called with a `key` argument or without arguments. In the latter case,
+        the decorator is used like this:
+
+        ```
+        @SeedableMixin.WithSeed
+        def func(...):
+            ...
+        ```
+
+        In this case, the name of the function is used as the key to the associated seed call. If a key is
+        provided, the decorator is used like this:
+
+        ```
+        @SeedableMixin.WithSeed(key="foo")
+        def func(...):
+            ...
+        ```
+
+        In this case, the key is used as the key to the associated seed call. This is useful when the function
+        name is not the desired seed.
+
+        Args:
+            fn: The function to wrap. This argument _does not need to be provided_ if a key is used; instead
+                the `doublewrap` decorator is used to allow the key to be passed as a keyword argument to a
+                meta-function that returns the true decorator applied to the target function.
+            key: The key to use for the seed. If None, the function name is used.
+
+        Returns:
+            A function that takes all the input arguments of the wrapped function and a seed keyword argument.
+            If the seed is not provided, a new seed is chosen. The seed is used to seed the RNG before calling
+            the wrapped function, under the provided key.
+
+        Note that if the function being wrapped explicitly takes a seed argument, this decorator will not
+        work, and the failure will not necessarily be graceful.
+
+        Examples:
+        """
         if key is None:
             key = fn.__name__
 
